5c53ee69b88ee531cf9a1a911ded5263f9a7cbbc
pclightyear <ee91608@gmail.com>
2021-02-09 14:52:30 +0800

    Development and Production mode switch
    Modify index.js file.
    Developer now can use config.json to determine development mode or production mode.
    Noted that normally we would setup two different environment. One for development and the other for production.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import './index.css';
 const config = require('./config.json');
 var AWS = require('aws-sdk');
 
-var awsRegion = config.awsRegion;
-var IdentityPoolId = config.IdentityPoolId;
-var BucketName = config.BucketName;
-var NameListFileKey = config.NameListFileKey;  
+if (config.DEVELOPMENT) {
+    var awsRegion = config.DEV.awsRegion;
+    var IdentityPoolId = config.DEV.IdentityPoolId;
+    var BucketName = config.DEV.BucketName;
+    var NameListFileKey = config.DEV.NameListFileKey;  
+} else {
+    var awsRegion = config.PROD.awsRegion;
+    var IdentityPoolId = config.PROD.IdentityPoolId;
+    var BucketName = config.PROD.BucketName;
+    var NameListFileKey = config.PROD.NameListFileKey;  
+};
 
 AWS.config.update({
     region: awsRegion,
